ea4a5e3a015788d1a9aa52687b3d796a5a6885ac
gorencnace <nace.gorenc@gmail.com>
2022-03-07 19:58:50 +0100

    ADD&CHANGE: Horn-Schunck, Lukas-Kanade

--- exercise1/ex1_utils.py
+++ exercise1/ex1_utils.py
@@ -6,11 +6,17 @@ from matplotlib.colors import hsv_to_rgb
 
 
 def gaussderiv(img, sigma):
-    x = np.array(list(range(math.floor(-3.0 * sigma + 0.5), math.floor(3.0 * sigma + 0.5) + 1)))
-    G = np.exp(-x ** 2 / (2 * sigma ** 2))
+    x = np.array(
+        list(range(math.floor(-3.0 * sigma + 0.5), math.floor(3.0 * sigma + 0.5) + 1))
+    )
+    G = np.exp(-(x ** 2) / (2 * sigma ** 2))
     G = G / np.sum(G)
 
-    D = -2 * (x * np.exp(-x ** 2 / (2 * sigma ** 2))) / (np.sqrt(2 * math.pi) * sigma ** 3)
+    D = (
+        -2
+        * (x * np.exp(-(x ** 2) / (2 * sigma ** 2)))
+        / (np.sqrt(2 * math.pi) * sigma ** 3)
+    )
     D = D / (np.sum(np.abs(D)) / 2)
 
     Dx = cv2.sepFilter2D(img, -1, D, G)
@@ -20,14 +26,16 @@ def gaussderiv(img, sigma):
 
 
 def gausssmooth(img, sigma):
-    x = np.array(list(range(math.floor(-3.0 * sigma + 0.5), math.floor(3.0 * sigma + 0.5) + 1)))
-    G = np.exp(-x ** 2 / (2 * sigma ** 2))
+    x = np.array(
+        list(range(math.floor(-3.0 * sigma + 0.5), math.floor(3.0 * sigma + 0.5) + 1))
+    )
+    G = np.exp(-(x ** 2) / (2 * sigma ** 2))
     G = G / np.sum(G)
     return cv2.sepFilter2D(img, -1, G, G)
 
 
-def show_flow(U, V, ax, type='field', set_aspect=False):
-    if type == 'field':
+def show_flow(U, V, ax, type="field", set_aspect=False):
+    if type == "field":
         scaling = 0.1
         u = cv2.resize(gausssmooth(U, 1.5), (0, 0), fx=scaling, fy=scaling)
         v = cv2.resize(gausssmooth(V, 1.5), (0, 0), fx=scaling, fy=scaling)
@@ -38,25 +46,35 @@ def show_flow(U, V, ax, type='field', set_aspect=False):
 
         ax.quiver(x, y, -u * 5, v * 5)
         if set_aspect:
-            ax.set_aspect(1.)
-    elif type == 'magnitude':
+            ax.set_aspect(1.0)
+    elif type == "magnitude":
         magnitude = np.sqrt(U ** 2 + V ** 2)
         ax.imshow(np.minimum(1, magnitude))
-    elif type == 'angle':
+    elif type == "angle":
         angle = np.arctan2(V, U) + math.pi
-        im_hsv = np.concatenate((np.expand_dims(angle / (2 * math.pi), -1),
-                                 np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1),
-                                 np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1)), axis=-1)
+        im_hsv = np.concatenate(
+            (
+                np.expand_dims(angle / (2 * math.pi), -1),
+                np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1),
+                np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1),
+            ),
+            axis=-1,
+        )
         ax.imshow(hsv_to_rgb(im_hsv))
-    elif type == 'angle_magnitude':
+    elif type == "angle_magnitude":
         magnitude = np.sqrt(U ** 2 + V ** 2)
         angle = np.arctan2(V, U) + math.pi
-        im_hsv = np.concatenate((np.expand_dims(angle / (2 * math.pi), -1),
-                                 np.expand_dims(np.minimum(1, magnitude), -1),
-                                 np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1)), axis=-1)
+        im_hsv = np.concatenate(
+            (
+                np.expand_dims(angle / (2 * math.pi), -1),
+                np.expand_dims(np.minimum(1, magnitude), -1),
+                np.expand_dims(np.ones(angle.shape, dtype=np.float32), -1),
+            ),
+            axis=-1,
+        )
         ax.imshow(hsv_to_rgb(im_hsv))
     else:
-        print('Error: unknown optical flow visualization type.')
+        print("Error: unknown optical flow visualization type.")
         exit(-1)
 
 
--- exercise1/of_methods.py
+++ exercise1/of_methods.py
@@ -1,35 +1,50 @@
-from exercise1.ex1_utils import gaussderiv, rotate_image, show_flow
+from random import gauss
+from ex1_utils import gaussderiv, rotate_image, show_flow, gausssmooth
 import numpy as np
 from scipy.signal import convolve2d
 import cv2
 
+
 def lucas_kanade(im1, im2, N):
-    Ix, Iy = gaussderiv(im1, 0.75)
-    It = im2 - im1
+    Ix1, Iy1 = gaussderiv(im1, 1)
+    Ix2, Iy2 = gaussderiv(im2, 1)
+    Ix = gausssmooth((Ix1 + Ix2) / 2, 1)
+    Iy = gausssmooth((Iy1 + Iy2) / 2, 1)
+    It = gausssmooth(im2 - im1, 1)
 
     kernel = np.ones((N, N))
 
-    Ix2 = convolve2d(Ix * Ix, kernel, 'same')
-    Iy2 = convolve2d(Iy * Iy, kernel, 'same')
-    IxIy = convolve2d(Ix * Iy, kernel, 'same')
-    IxIt = convolve2d(Ix * It, kernel, 'same')
-    IyIt = convolve2d(Iy * It, kernel, 'same')
+    IxIx = convolve2d(Ix * Ix, kernel, "same")
+    IyIy = convolve2d(Iy * Iy, kernel, "same")
+    IxIy = convolve2d(Ix * Iy, kernel, "same")
+    IxIt = convolve2d(Ix * It, kernel, "same")
+    IyIt = convolve2d(Iy * It, kernel, "same")
 
-    D = Ix2 * Iy2 - IxIy ** 2
-
-    U = (IxIy * IyIt - Iy2 * IxIt) / D
-    V = (IxIy * IxIt - Ix2 * IyIt) / D
-
+    D = IxIx * IyIy - IxIy ** 2 + 0.0001
+    U = (IxIy * IyIt - IyIy * IxIt) / D
+    V = (IxIy * IxIt - IxIx * IyIt) / D
     return U, V
 
 
 def horn_schunck(im1, im2, n_iters, lmbd):
-    pass
+    Ld = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) / 4
+    u, v = np.zeros_like(im1), np.zeros_like(im1)
+    Ix1, Iy1 = gaussderiv(im1, 1)
+    Ix2, Iy2 = gaussderiv(im2, 1)
+    Ix = gausssmooth((Ix1 + Ix2) / 2, 1)
+    Iy = gausssmooth((Iy1 + Iy2) / 2, 1)
+    It = gausssmooth(im2 - im1, 1)
 
+    D = lmbd + Ix * Ix + Iy * Iy
 
-if __name__ == '__main__':
-    path = '.\\exercise1\\lab2\\'
-    img1 = cv2.imread(path + '001.jpg', 0)
-    img2 = rotate_image(img1, 5)
-    u, v = lucas_kanade(img1, img2, 3)
-    show_flow(u, v)
+    for _ in range(n_iters):
+        ua = convolve2d(u, Ld, "same")
+        va = convolve2d(v, Ld, "same")
+
+        P = Ix * ua + Iy * va + It
+
+        pd = P / D
+        u = ua - Ix * pd
+        v = va - Iy * pd
+
+    return u, v
