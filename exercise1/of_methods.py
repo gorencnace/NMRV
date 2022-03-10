f5b8501b40505d2672e255b05ca583f415239fe6
gorencnace <nace.gorenc@gmail.com>
2022-03-10 23:12:47 +0100

    HS is good but LK is fed big time

--- exercise1/of_methods.py
+++ exercise1/of_methods.py
@@ -1,41 +1,65 @@
 from random import gauss
 from ex1_utils import gaussderiv, rotate_image, show_flow, gausssmooth
 import numpy as np
-from scipy.signal import convolve2d
 import cv2
+from scipy.signal import convolve2d
 
 
 def lucas_kanade(im1, im2, N):
+    threshold = 0.5
     Ix1, Iy1 = gaussderiv(im1, 1)
     Ix2, Iy2 = gaussderiv(im2, 1)
-    Ix = gausssmooth((Ix1 + Ix2) / 2, 1)
-    Iy = gausssmooth((Iy1 + Iy2) / 2, 1)
+    Ix = (Ix1 + Ix2) / 2
+    Iy = (Iy1 + Iy2) / 2
     It = gausssmooth(im2 - im1, 1)
 
     kernel = np.ones((N, N))
 
-    IxIx = convolve2d(Ix * Ix, kernel, "same")
-    IyIy = convolve2d(Iy * Iy, kernel, "same")
+    IxIx = convolve2d(Ix ** 2, kernel, "same")
+    IyIy = convolve2d(Iy ** 2, kernel, "same")
     IxIy = convolve2d(Ix * Iy, kernel, "same")
     IxIt = convolve2d(Ix * It, kernel, "same")
     IyIt = convolve2d(Iy * It, kernel, "same")
 
-    D = IxIx * IyIy - IxIy ** 2 + 0.0001
-    U = (IxIy * IyIt - IyIy * IxIt) / D
-    V = (IxIy * IxIt - IxIx * IyIt) / D
+    D = IxIx * IyIy - IxIy ** 2
+    T = IxIx + IyIy
+    k = 0.05
+    r = D - k * T
+    r[r >= threshold] = 1
+    r[r < threshold] = 0
+
+    D[D == 0] = 1000
+
+    U = (IxIy * IyIt - IyIy * IxIt) * r / D
+    V = (IxIy * IxIt - IxIx * IyIt) * r / D
+
     return U, V
 
 
 def horn_schunck(im1, im2, n_iters, lmbd):
-    Ld = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) / 4
-    u, v = np.zeros_like(im1), np.zeros_like(im1)
-    Ix1, Iy1 = gaussderiv(im1, 1)
-    Ix2, Iy2 = gaussderiv(im2, 1)
-    Ix = gausssmooth((Ix1 + Ix2) / 2, 1)
-    Iy = gausssmooth((Iy1 + Iy2) / 2, 1)
-    It = gausssmooth(im2 - im1, 1)
+    im1 = gausssmooth(im1 / 255, 1)
+    im2 = gausssmooth(im2 / 255, 1)
 
-    D = lmbd + Ix * Ix + Iy * Iy
+    x_kernel = np.array([[-1, 1], [-1, 1]]) * 0.5
+    y_kernel = np.array([[-1, -1], [1, 1]]) * 0.5
+    t_kernel = np.ones((2, 2)) * 0.25
+    Ld = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]]) * 0.25
+
+    u, v = np.zeros_like(im1), np.zeros_like(im1)
+
+    Ix = convolve2d(im1, x_kernel, "same") + convolve2d(im2, x_kernel, "same")
+    # cv2.imshow("", Ix)
+    # cv2.waitKey(0)
+
+    Iy = convolve2d(im1, y_kernel, "same") + convolve2d(im2, y_kernel, "same")
+    # cv2.imshow("", Iy)
+    # cv2.waitKey(0)
+
+    It = convolve2d(im2, t_kernel, "same") - convolve2d(im1, t_kernel, "same")
+    # cv2.imshow("", It)
+    # cv2.waitKey(0)
+
+    D = lmbd + Ix ** 2 + Iy ** 2
 
     for _ in range(n_iters):
         ua = convolve2d(u, Ld, "same")
--- exercise1/run_vaja_1.py
+++ exercise1/run_vaja_1.py
@@ -4,45 +4,63 @@ from ex1_utils import rotate_image, show_flow
 from of_methods import lucas_kanade, horn_schunck
 import cv2
 
-"""
-im1 = np.random.rand(200, 200).astype(np.float32)
+
+im1 = np.random.rand(10, 10).astype(np.float32)
 im2 = im1.copy()
-im2 = rotate_image(im2, 1)
+im2 = rotate_image(im2, -1)
 
-U_lk, V_lk = lucas_kanade(im1, im2, 3)
-U_hs, V_hs = horn_schunck(im1, im2, 1000, 0.5)
+
+path = ".\\exercise1\\disparity\\"
+im1 = cv2.imread(path + "office_left.png", 0)
+im2 = cv2.imread(path + "office_right.png", 0)
+
+
+U_lk, V_lk = lucas_kanade(im1, im2, 5)
 fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22)) = plt.subplots(2, 2)
 ax1_11.imshow(im1)
 ax1_12.imshow(im2)
 show_flow(U_lk, V_lk, ax1_21, type="angle")
 show_flow(U_lk, V_lk, ax1_22, type="field", set_aspect=True)
-fig1.suptitle(" Lucas−Kanade Optical Flow ")
+fig1.suptitle("Lucas−Kanade Optical Flow")
+
+U_hs, V_hs = horn_schunck(im1, im2, 500, 0.5)
 fig2, ((ax2_11, ax2_12), (ax2_21, ax2_22)) = plt.subplots(2, 2)
 ax2_11.imshow(im1)
 ax2_12.imshow(im2)
 show_flow(U_hs, V_hs, ax2_21, type="angle")
 show_flow(U_hs, V_hs, ax2_22, type="field", set_aspect=True)
-fig2.suptitle("Horn−Schunck Optical Flow ")
+fig2.suptitle("Horn−Schunck Optical Flow")
+
 """
-
-path = ".\\exercise1\\collision\\"
-im1 = cv2.imread(path + "00000161.jpg", 0)
-im2 = cv2.imread(path + "00000162.jpg", 0)
-
-U_lk, V_lk = lucas_kanade(im1, im2, 3)
-U_hs, V_hs = horn_schunck(im1, im2, 1000, 0.5)
+U_1, V_1 = lucas_kanade(im1, im2, 3)
+U_2, V_2 = lucas_kanade(im1, im2, 9)
 fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22)) = plt.subplots(2, 2)
 ax1_11.imshow(im1)
 ax1_12.imshow(im2)
-show_flow(U_lk, V_lk, ax1_21, type="angle")
-show_flow(U_lk, V_lk, ax1_22, type="field", set_aspect=True)
-fig1.suptitle(" Lucas−Kanade Optical Flow ")
+show_flow(U_1, V_1, ax1_21, type="angle")
+show_flow(U_1, V_1, ax1_22, type="field", set_aspect=True)
+fig1.suptitle("Lucas−Kanade Optical Flow n=3")
 fig2, ((ax2_11, ax2_12), (ax2_21, ax2_22)) = plt.subplots(2, 2)
 ax2_11.imshow(im1)
 ax2_12.imshow(im2)
-show_flow(U_hs, V_hs, ax2_21, type="angle")
-show_flow(U_hs, V_hs, ax2_22, type="field", set_aspect=True)
-fig2.suptitle("Horn−Schunck Optical Flow ")
+show_flow(U_2, V_2, ax2_21, type="angle")
+show_flow(U_2, V_2, ax2_22, type="field", set_aspect=True)
+fig2.suptitle("Lucas−Kanade Optical Flow n=9")
 
+U_1, V_1 = horn_schunck(im1, im2, 100, 0.5)
+U_2, V_2 = horn_schunck(im1, im2, 1000, 0.5)
+fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22)) = plt.subplots(2, 2)
+ax1_11.imshow(im1)
+ax1_12.imshow(im2)
+show_flow(U_1, V_1, ax1_21, type="angle")
+show_flow(U_1, V_1, ax1_22, type="field", set_aspect=True)
+fig1.suptitle("Horn-Schunck Optical Flow n_iter=100")
+fig2, ((ax2_11, ax2_12), (ax2_21, ax2_22)) = plt.subplots(2, 2)
+ax2_11.imshow(im1)
+ax2_12.imshow(im2)
+show_flow(U_2, V_2, ax2_21, type="angle")
+show_flow(U_2, V_2, ax2_22, type="field", set_aspect=True)
+fig2.suptitle("Horn-Schunck Optical Flow n_iter=1000")
+"""
 
 plt.show()
